e6490d3fea018ff79d547a74a33c36a6b15ab56a
Manuel Gutiérrez Rivas <89094000+manugutier@users.noreply.github.com>
2025-02-17 13:21:11 -0600

    feat (quests): add quests manager

---
+++ Assets/Scripts/MVC/Controller/QuestManager.cs
@@ -0,0 +1,33 @@
+using System.Collections.Generic;
+using manulogics.Signals;
+using Zenject;
+
+namespace manulogics.Quests
+{
+    public class QuestManager
+    {
+        private List<IQuest> _activeQuests = new();
+        private SignalBus _signalBus;
+
+        [Inject]
+        public QuestManager(SignalBus signalBus)
+        {
+            _signalBus = signalBus;
+            signalBus.Subscribe<ItemCollectedSignal>(OnItemCollected);
+        }
+
+        public void AddQuest(IQuest quest)
+        {
+            _activeQuests.Add(quest);
+        }
+
+        private void OnItemCollected(ItemCollectedSignal signal)
+        {
+            foreach (var quest in _activeQuests)
+            {
+                if (quest is CollectItem_Quest collectQuest)
+                    collectQuest.OnItemCollected(signal.Item);
+            }
+        }
+    }
+}
