5e7f8d715fd8e5ae3054a72dd7868c1734be159f
Manuel Gutiérrez Rivas <89094000+manugutier@users.noreply.github.com>
2025-02-24 00:03:25 -0600

    chore (installers): quest debug ui binding in game installer

--- Assets/Scripts/DI/GameInstaller.cs
+++ Assets/Scripts/DI/GameInstaller.cs
@@ -1,15 +1,20 @@
 using manulogics.Quests;
 using manulogics.Signals;
+using manulogics.UI;
+using UnityEngine;
 using Zenject;
 
 namespace manulogics.Installers
 {
     public class GameInstaller : MonoInstaller
     {
+        [SerializeField] private QuestDebugUI questDebugUI;
+        
         public override void InstallBindings()
         {
             Container.BindInterfacesTo<QuestManager>().AsSingle();
             QuestInstaller.Install(Container);
+            Container.Bind<QuestDebugUI>().FromInstance(questDebugUI).AsSingle();
         }
     }
 }
