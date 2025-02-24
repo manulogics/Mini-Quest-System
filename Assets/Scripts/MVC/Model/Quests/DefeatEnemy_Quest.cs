f5cb7c53685cf425abc43b60cb2ba64e3de22589
Manuel Gutiérrez Rivas <89094000+manugutier@users.noreply.github.com>
2025-02-24 00:05:34 -0600

    chore (enemy): add on enemy defeated method in DefeatEnemy_Quest.cs

--- Assets/Scripts/MVC/Model/Quests/DefeatEnemy_Quest.cs
+++ Assets/Scripts/MVC/Model/Quests/DefeatEnemy_Quest.cs
@@ -1,14 +1,17 @@
+using manulogics.Enemies;
 using UnityEngine;
 
 namespace manulogics.Quests
 {
     public class DefeatEnemy_Quest : Quest
     {
+        private EnemyType _enemyType;
         private int _requiredAmount;
         private int _currentAmount;
         
-        public DefeatEnemy_Quest(string title, int amount) : base(title)
+        public DefeatEnemy_Quest(string title, EnemyType enemyType, int amount) : base(title)
         {
+            _enemyType = enemyType;
             _requiredAmount = amount;
             _currentAmount = 0;
         }
@@ -24,6 +27,15 @@ namespace manulogics.Quests
             {
                 IsCompleted = true;
                 Debug.Log($"Quest Completed: {Title}");
+            }
+        }
+
+        public void OnEnemyDefeated(Enemy enemy)
+        {
+            if (enemy.Type == _enemyType)
+            {
+                _currentAmount++;
+                CheckProgress();
             }
         }
     }
